15a335db06910a8feb9385ee82444641132b8e11
Kenry Wan Nainggolan <kenry.nainggolan@gmail.com>
2015-04-10 08:57:26 +0700

    Tambah Model mModule
    Untuk Menu

---
+++ protected/models/mModule.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * This is the model class for table "m_module".
+ *
+ * The followings are the available columns in table 'm_module':
+ * @property integer $id
+ * @property integer $parent_id
+ * @property integer $sort
+ * @property string $name
+ * @property string $title
+ * @property string $description
+ * @property string $link
+ * @property string $image
+ * @property integer $created_date
+ * @property integer $created_by
+ * @property integer $updated_date
+ * @property integer $updated_by
+ */
+class mModule extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'm_module';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('parent_id, sort, title', 'required'),
+			array('parent_id, sort, created_date, created_by, updated_date, updated_by', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>15),
+			array('title, image', 'length', 'max'=>50),
+			array('description', 'length', 'max'=>100),
+			array('link', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, parent_id, sort, name, title, description, link, image, created_date, created_by, updated_date, updated_by', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'parent_id' => 'Parent',
+			'sort' => 'Sort',
+			'name' => 'Name',
+			'title' => 'Title',
+			'description' => 'Description',
+			'link' => 'Link',
+			'image' => 'Image',
+			'created_date' => 'Created Date',
+			'created_by' => 'Created By',
+			'updated_date' => 'Updated Date',
+			'updated_by' => 'Updated By',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('parent_id',$this->parent_id);
+		$criteria->compare('sort',$this->sort);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('link',$this->link,true);
+		$criteria->compare('image',$this->image,true);
+		$criteria->compare('created_date',$this->created_date);
+		$criteria->compare('created_by',$this->created_by);
+		$criteria->compare('updated_date',$this->updated_date);
+		$criteria->compare('updated_by',$this->updated_by);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return mModule the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
